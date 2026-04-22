439977596c3775a593511d06cdb8ea245f526fe4
Your Name <your-email@gmail.com>
2026-04-22 21:56:14 +0600

    fixing case sensitivity for NotFound

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
+import App from '../Pages/App';
 import Home from '../Pages/Home';
 import Timeline from '../Pages/Timeline';
 import Stats from '../Pages/Stats';
